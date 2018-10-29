6f0c71ca764058c7eecd404303b5f8dc2eeeea81
musicode <musicode@qq.com>
2018-10-29 23:37:46 +0800

    update

--- library/src/main/java/com/github/herokotlin/voiceinput/VoiceInput.kt
+++ library/src/main/java/com/github/herokotlin/voiceinput/VoiceInput.kt
@@ -5,7 +5,6 @@ import android.os.Handler
 import android.os.Message
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
